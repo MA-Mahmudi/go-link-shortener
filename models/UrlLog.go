3e001f7209fd4693a6a37cfe2ef0b617697ce3fb
ma-mahmudi <ma.mahmudi18@outlook.com>
2024-02-28 08:05:42 +0330

    ID changed to Id

--- models/UrlLog.go
+++ models/UrlLog.go
@@ -7,7 +7,7 @@ import (
 )
 
 type UrlLog struct {
-	ID         primitive.ObjectID `json:"id" bson:"_id"`
+	Id         primitive.ObjectID `json:"id" bson:"_id"`
 	UrlId      primitive.ObjectID `json:"url_id" bson:"url_id"`
 	ClickCount int                `json:"click_count" bson:"click_count"`
 	UserAgent  string             `json:"user_agent" bson:"user_agent"`
