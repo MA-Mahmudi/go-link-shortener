2a330066937c7c78feddc84a8b732972069e7acd
ma-mahmudi <ma.mahmudi18@outlook.com>
2024-03-04 15:10:06 +0330

    Updated structs

--- models/Url.go
+++ models/Url.go
@@ -10,12 +10,18 @@ type Url struct {
 	Id          primitive.ObjectID `json:"id" bson:"_id"`
 	Title       string             `json:"title" bson:"title"`
 	ShortLink   string             `json:"short_link" bson:"short_link"`
-	OriginalUrl string             `json:"original_url" bson:"original_url" validate:"required"`
+	OriginalUrl string             `json:"original_url" bson:"original_url"`
 	CreatedAt   string             `json:"created_at" bson:"created_at"`
 	UpdatedAt   string             `json:"updated_at" bson:"updated_at"`
 	DeletedAt   string             `json:"deleted_at" bson:"deleted_at"`
 	ExpireDate  string             `json:"expire_date" bson:"expire_date"`
 	Status      int                `json:"status" bson:"status"`
+}
+
+type CreateUrlStruct struct {
+	Title       string `json:"title" bson:"title"`
+	OriginalUrl string `json:"original_url" bson:"original_url" validate:"required"`
+	CustomUrl   string `json:"custom_url" bson:"custom_url"`
 }
 
 var UrlsCollection *mongo.Collection = configs.GetCollection(configs.GetDBClint(), "urls")
