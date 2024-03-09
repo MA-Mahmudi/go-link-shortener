eebfe6d438dad3cc5a19db4280ba1468579bbf9e
ma-mahmudi <ma.mahmudi18@outlook.com>
2024-03-09 09:39:03 +0330

    Renamed

--- controllers/create.go
+++ controllers/create.go
@@ -2,12 +2,10 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"github.com/MrMohebi/golang-gin-boilerplate.git/common"
 	"github.com/MrMohebi/golang-gin-boilerplate.git/constant"
 	"github.com/MrMohebi/golang-gin-boilerplate.git/models"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 	"time"
@@ -67,26 +65,4 @@ func CreateUrl() gin.HandlerFunc {
 		}
 	}
 
-}
-
-func GetAllUrls() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		var urls []models.Url
-
-		count, _ := models.UrlsCollection.CountDocuments(ctx, bson.M{})
-		res, err := models.UrlsCollection.Find(ctx, bson.M{})
-		if err != nil {
-			common.IsErr(err, false)
-			c.JSON(http.StatusInternalServerError, err)
-		} else {
-			fmt.Println(res.All(ctx, &urls))
-			c.JSON(http.StatusOK, gin.H{
-				"count": count,
-				"date":  urls,
-			})
-		}
-	}
 }
